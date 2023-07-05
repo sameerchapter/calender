27f225b963b6232651b2d28621420452651780de
Sameer khan <sameer.k@chapter247.com>
2023-07-06 00:29:52 +0530

    send notification

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DeviceToken;
 use Illuminate\Http\Request;
 use App\Models\Staff;
 use App\Models\User;
@@ -77,45 +78,49 @@ class StaffController extends Controller
 
     public function send_notification()
     {
-        $payload = array(
-            'to' => 'ExponentPushToken[xxborxxxxxxxxxx]',
-            'sound' => 'default',
-            'body' => 'hello',
-        );
+        $tokens = DeviceToken::all();
+        foreach ($tokens as $token) {
+            $payload = array(
+                'to' => $token->token,
+                'sound' => 'default',
+                'body' => 'hello',
+            );
 
-        $curl = curl_init();
+            $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://exp.host/--/api/v2/push/send",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($payload),
-            CURLOPT_HTTPHEADER => array(
-                "Accept: application/json",
-                "Accept-Encoding: gzip, deflate",
-                "Content-Type: application/json",
-                "cache-control: no-cache",
-                "host: exp.host"
-            ),
-        ));
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://exp.host/--/api/v2/push/send",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode($payload),
+                CURLOPT_HTTPHEADER => array(
+                    "Accept: application/json",
+                    "Accept-Encoding: gzip, deflate",
+                    "Content-Type: application/json",
+                    "cache-control: no-cache",
+                    "host: exp.host"
+                ),
+            ));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
 
-        curl_close($curl);
+            curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                echo $response;
+            }
         }
     }
 
-    public function notification(){
+    public function notification()
+    {
         return view('notification');
     }
 }
