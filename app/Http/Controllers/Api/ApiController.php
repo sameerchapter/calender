0ad845e998bab7b4bae712a6f35c64bbe0ffd7f3
sameerthor <sameer@thor.solutions>
2023-12-05 00:56:48 +0530

    staff issue

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -149,7 +149,7 @@ class ApiController extends Controller
 
     public function bookingData(Request $request)
     {
-        // try {
+         try {
             $validateUser = Validator::make(
                 $request->all(),
                 [
@@ -170,24 +170,24 @@ class ApiController extends Controller
             $booking_data = $booking->BookingData->sortBy('department_id');
             $data['bcn'] = !empty($booking->bcn) ? $booking->bcn : "N/A";
             $data['address'] = $booking->address;
-            $data['building_company'] = $booking_data[0]->department_id == "1" ? $booking_data[0]->contact->title : "NA";
+             $data['building_company'] = $booking_data[0]->department_id == "1" ? $booking_data[0]->contact->title : "NA";
             $data['floor_type'] = !empty($booking->floor_type) ? $booking->floor_type : "N/A";
             $data['floor_area'] = !empty($booking->floor_area) ? $booking->floor_area : "N/A";
             $data['booking_notes'] = !empty($booking->notes) ? $booking->notes : "N/A";
-            $data['notes'] = !empty($schedule->notes) ? $schedule->notes : "N/A";
-            $data['staff_assigned'] = implode(" • ", Staff::whereIn('id', $schedule->staff_id)->get()->pluck('name')->toArray());
-            $data['foreman_assigned'] = $schedule->foreman->name;
+             $data['notes'] = !empty($schedule->notes) ? $schedule->notes : "N/A";
+             $data['staff_assigned'] = implode(" • ", Staff::whereIn('id', is_array($schedule->staff_id) && !empty($schedule->staff_id) ?$schedule->staff_id:[])->get()->pluck('name')->toArray());
+             $data['foreman_assigned'] = $schedule->foreman->name;
 
             return response()->json([
                 'status' => true,
                 'data' => $data
             ], 200, [], JSON_UNESCAPED_UNICODE);
-        // } catch (\Throwable $th) {
-        //     return response()->json([
-        //         'status' => false,
-        //         'message' => $th->getMessage()
-        //     ], 500);
-        // }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     public function saveToken(Request $request)
